d8f9cd9f090be496acefa5e2233c3e3aea8a36e8
Josh Weaver <jweaver911@gmail.com>
2020-06-24 20:05:31 -0400

    adds initial Group unit test

---
+++ tests/Api/GroupTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GroupControllerTest extends TestCase
+{
+    use WithFaker;
+
+    public function testGroupCreated()
+    {
+        $expected = [
+            'group_name' => 'Volunteers'
+        ];
+        $response = $this->json('POST', '/api/group', $expected);
+        $response
+            ->assertStatus(201)
+            ->assertJsonFragment($expected);
+
+    }
+}
