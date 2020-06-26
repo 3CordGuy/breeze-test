1f845de649f730371091a39d3e8085566e91e091
Josh Weaver <jweaver911@gmail.com>
2020-06-26 13:38:48 -0400

    adds group members list column sorting

--- src/Groups/GroupMembersList.js
+++ src/Groups/GroupMembersList.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Table, Header, Button, Icon } from "semantic-ui-react";
+import { Table, Header, Button, Icon, TableRow } from "semantic-ui-react";
 import _ from "lodash";
 
 class GroupMembersList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             group: "Group",
             members: [],
             column: "last_name",
@@ -36,15 +37,16 @@ class GroupMembersList extends Component {
         let { group_id } = this.props;
         fetch(`http://localhost:8000/api/groups/${group_id}`)
             .then((response) => response.json())
-            .then(({ data }) =>
+            .then(({ data }) => {
                 this.setState({
+                    loading: false,
                     members: _.sortBy(data.members, [
                         "last_name",
                         "descending",
                     ]),
                     group: data.name,
-                }),
-            );
+                });
+            });
     }
 
     render() {
@@ -56,7 +58,13 @@ class GroupMembersList extends Component {
                 <Table celled padded sortable>
                     <Table.Header>
                         <Table.Row>
-                            <Table.HeaderCell singleLine>
+                            <Table.HeaderCell
+                                singleLine
+                                sorted={
+                                    column === "first_name" ? direction : null
+                                }
+                                onClick={this.handleSort("first_name")}
+                            >
                                 First Name
                             </Table.HeaderCell>
                             <Table.HeaderCell
@@ -68,39 +76,71 @@ class GroupMembersList extends Component {
                             >
                                 Last Name
                             </Table.HeaderCell>
-                            <Table.HeaderCell>Email</Table.HeaderCell>
+                            <Table.HeaderCell
+                                sorted={column === "email" ? direction : null}
+                                onClick={this.handleSort("email")}
+                            >
+                                Email
+                            </Table.HeaderCell>
                             <Table.HeaderCell>Status</Table.HeaderCell>
                             <Table.HeaderCell></Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
 
                     <Table.Body>
-                        {members.map((person, index) => {
-                            return (
-                                <Table.Row key={index}>
-                                    <Table.Cell singleLine>
-                                        {person.first_name}
-                                    </Table.Cell>
-                                    <Table.Cell singleLine>
-                                        {person.last_name}
-                                    </Table.Cell>
-                                    <Table.Cell singleLine>
-                                        {person.email_address}
-                                    </Table.Cell>
-                                    <Table.Cell singleLine>
-                                        {person.status}
-                                    </Table.Cell>
-                                    <Table.Cell singleLine textAlign="center">
-                                        <Button icon circular primary>
-                                            <Icon name="edit" />
-                                        </Button>
-                                        <Button icon negative basic circular>
-                                            <Icon name="trash" />
-                                        </Button>
-                                    </Table.Cell>
-                                </Table.Row>
-                            );
-                        })}
+                        {members.length ? (
+                            members.map((person, index) => {
+                                return (
+                                    <Table.Row key={index}>
+                                        <Table.Cell singleLine>
+                                            {person.first_name}
+                                        </Table.Cell>
+                                        <Table.Cell singleLine>
+                                            {person.last_name}
+                                        </Table.Cell>
+                                        <Table.Cell singleLine>
+                                            {person.email_address}
+                                        </Table.Cell>
+                                        <Table.Cell singleLine>
+                                            {person.status}
+                                        </Table.Cell>
+                                        <Table.Cell
+                                            singleLine
+                                            textAlign="center"
+                                        >
+                                            <Button icon circular primary>
+                                                <Icon name="edit" />
+                                            </Button>
+                                            <Button
+                                                icon
+                                                negative
+                                                basic
+                                                circular
+                                            >
+                                                <Icon name="trash" />
+                                            </Button>
+                                        </Table.Cell>
+                                    </Table.Row>
+                                );
+                            })
+                        ) : (
+                            <Table.Row>
+                                <Table.Cell textAlign="center">
+                                    This Group has No Members{" "}
+                                    <span
+                                        role="img"
+                                        aria-label="sad face"
+                                        style={{ fontSize: "2rem" }}
+                                    >
+                                        🙁
+                                    </span>
+                                </Table.Cell>
+                                <Table.Cell />
+                                <Table.Cell />
+                                <Table.Cell />
+                                <Table.Cell />
+                            </Table.Row>
+                        )}
                     </Table.Body>
                 </Table>
             </>
