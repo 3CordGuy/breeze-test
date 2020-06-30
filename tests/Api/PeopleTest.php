25323fe50c2a6f0981e4ed54718d08a103776210
Josh Weaver <jweaver911@gmail.com>
2020-06-29 21:25:03 -0400

    adds csv people import endpoint tests

--- tests/Api/PeopleTest.php
+++ tests/Api/PeopleTest.php
@@ -89,4 +89,81 @@ class PeopleControllerTest extends TestCase
         $response = $this->json('GET', '/api/people/' . $person->id);
         $response->assertStatus(404);
     }
+
+    public function testPeopleImport()
+    {
+        $peopleData = [
+            [
+                "id" => 14,
+                "first_name" => "Leslie",
+                "last_name" => "Knope",
+                "email_address" => "leslie@pawnee.in.gov",
+                "status" => "active"
+            ],
+            [
+                "id" => 2,
+                "first_name" => "Jerry",
+                "last_name" => "Gergich",
+                "email_address" => "garry@pawnee.in.gov",
+                "status" => "active"
+            ],
+        ];
+
+        $deleteResponse = $this->json('POST', '/api/people-import/', $peopleData);
+        $deleteResponse->assertStatus(204);
+
+        $response = $this->json('GET', '/api/people/');
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(2, 'data');
+    }
+
+    public function testPeopleImportCanUpdate()
+    {
+        // Initial insert nature of the endpoint
+        $peopleData1 = [
+            [
+                "id" => 14,
+                "first_name" => "Leslie",
+                "last_name" => "Knope",
+                "email_address" => "leslie@pawnee.in.gov",
+                "status" => "active"
+            ],
+            [
+                "id" => 2,
+                "first_name" => "Jerry",
+                "last_name" => "Gergich",
+                "email_address" => "garry@pawnee.in.gov",
+                "status" => "active"
+            ]
+        ];
+
+        $importResponse = $this->json('POST', '/api/people-import/', $peopleData1);
+        $importResponse->assertStatus(204);
+
+        // We will change Jerry to Larry and test the upsert nature of the endpoint
+        $peopleData2 = [
+            [
+                "id" => 16,
+                "first_name" => "Andy",
+                "last_name" => "Dwyer",
+                "email_address" => "andy@johnnykarate.com",
+                "status" => "active"
+            ],
+            [
+                "id" => 2,
+                "first_name" => "Larry",
+                "last_name" => "Gergich",
+                "email_address" => "garry@pawnee.in.gov",
+                "status" => "active"
+            ]
+        ];
+
+
+        $importResponse = $this->json('POST', '/api/people-import/', $peopleData2);
+        $importResponse->assertStatus(204);
+
+        $updatedPerson = Person::find(2);
+        $this->assertEquals("Larry", $updatedPerson->first_name);
+    }
 }
